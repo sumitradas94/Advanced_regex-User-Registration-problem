37726524873442c4024d3353b26981a64ac8c78c
sumitradas94 <sumitradas94@gmail.com>
2023-01-27 19:59:08 +0530

    Check valid first name

---
+++ Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Pattern
+    {
+
+        public static string REGEX_PAT = "^[A-Z]{1}[a-zA-Z0-9]{2,20}$";
+        public static bool validateName(string name)
+        {
+            return Regex.IsMatch(name, REGEX_PAT);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using UserRegistrationProblem;
+
+namespace UserRegistrarionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the First Name : ");
+            string name = Console.ReadLine();
+            bool val = Pattern.validateName(name);
+            if (val)
+            {
+                Console.WriteLine("First Name is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not Valid.");
+
+            }
+
+        }
+    }
+}
