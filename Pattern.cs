20df3c506db3f14988d5ed3873a2345aef1fb7cc
sumitradas94 <sumitradas94@gmail.com>
2023-01-27 22:09:01 +0530

    Clear all email samples

--- Pattern.cs
+++ Pattern.cs
@@ -9,15 +9,70 @@ namespace UserRegistrationProblem
 {
     public class Pattern
     {
-        public static string Reg_pattern8 = "^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$";
 
-        public static bool validatePassword4(string password)
+        public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)?@(bl).(co)([.][a-z]{2,})?$";
+        public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+        public static string Reg_pattern5 = "^[a-zA-Z]{8,}([0-9]+)?$";
+        public static string Reg_pattern6 = "^[A-Z]{1,}[a-zA-Z]{7,}([0-9]+)?$";
+        public static string Reg_pattern7 = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$";
+        public static string Reg_pattern8 = "^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$";
+        public static string Reg_pattern9 = "^[A-Za-z0-9]+([.+-][A-Za-z0-9]+)*@[A-Za-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$";
+
+
+        //abc.xyz@bl.co.in
+
+
+        public static bool validateName(string name)
+        {
+            return Regex.IsMatch(name, Reg_pattern1);
+
+        }
+        public static bool validateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, Reg_pattern2);
+        }
+
+        public static bool validateEmailID(string email)
+        {
+            return Regex.IsMatch(email, Reg_pattern3);
+        }
+        public static bool validateMobileNumber(string Monumber)
+        {
+            return Regex.IsMatch(Monumber, Reg_pattern4);
+
+        }
+
+        public static bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Reg_pattern5);
+
+        }
+
+        public static bool validatePassword1(string password)
+        {
+            return Regex.IsMatch(password, Reg_pattern6);
+
+        }
+
+        public static bool validatePassword2(string password)
+        {
+            return Regex.IsMatch(password, Reg_pattern7);
+
+        }
+
+        public static bool validatePassword3(string password)
         {
             return Regex.IsMatch(password, Reg_pattern8);
 
         }
 
+        public static bool validateEmailID1(string email)
+        {
+            return Regex.IsMatch(email, Reg_pattern9);
+        }
+
     }
 }
-
 
--- Program.cs
+++ Program.cs
@@ -6,11 +6,67 @@ namespace UserRegistrarionProblem
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the First Name : ");
+            string name = Console.ReadLine();
+            bool val = Pattern.validateName(name);
+
+            Console.WriteLine("Please enter the Last Name : ");
+            string lastname = Console.ReadLine();
+            bool val2 = Pattern.validateLastName(lastname);
+
+            Console.WriteLine("Please enter the Email  ID : ");
+            string email = Console.ReadLine();
+            bool val3 = Pattern.validateEmailID(email);
+
+            Console.WriteLine("Please enter the Mobile Number : ");
+            string Monumber = Console.ReadLine();
+            bool val4 = Pattern.validateMobileNumber(Monumber);
+
             Console.WriteLine("Please enter the Password : ");
             string password = Console.ReadLine();
-            bool val8 = Pattern.validatePassword4(password);
+            bool val5 = Pattern.validatePassword(password);
 
-            if (val8)
+            if (val)
+            {
+                Console.WriteLine("First Name is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not Valid.");
+
+            }
+
+            if (val2)
+            {
+                Console.WriteLine("Last Name is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Last Name is not Valid.");
+
+            }
+
+            if (val3)
+            {
+                Console.WriteLine("Email ID is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Email ID is not Valid.");
+
+            }
+
+            if (val4)
+            {
+                Console.WriteLine("Mobile Number is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Mobile Number is not Valid.");
+
+            }
+
+            if (val5)
             {
                 Console.WriteLine("Password is Correct.");
             }
@@ -19,9 +75,6 @@ namespace UserRegistrarionProblem
                 Console.WriteLine("Password is not Correct.");
 
             }
-
-
-
 
         }
     }
